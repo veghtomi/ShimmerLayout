8a9f2fff9dbb59f437635c21421a3864fb6c72ce
veghtomi <tommi.vegh@gmail.com>
2018-02-06 14:50:57 +0100

    Fix id missing issue

--- app/src/main/java/io/supercharge/shimmeringlayout/tests/MaskCenterColorWidthTestsActivity.java
+++ app/src/main/java/io/supercharge/shimmeringlayout/tests/MaskCenterColorWidthTestsActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import io.supercharge.shimmeringlayout.R;
-import io.supercharge.shimmerlayout.ShimmerLayout;
 
 public class MaskCenterColorWidthTestsActivity extends AppCompatActivity {
 
@@ -12,14 +11,5 @@ public class MaskCenterColorWidthTestsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mask_center_color_width_tests);
-
-        ShimmerLayout maskWidth1ShimmerLayout = findViewById(R.id.mask_center_color_width_test_mask_1_from_code);
-        maskWidth1ShimmerLayout.setMaskWidth(1);
-        maskWidth1ShimmerLayout.setGradientCenterColorWidth(0.4F);
-        maskWidth1ShimmerLayout.startShimmerAnimation();
-
-        ShimmerLayout defaultCenterColorWidth = findViewById(R.id.mask_center_color_width_test_center_color_default_from_code);
-        defaultCenterColorWidth.setMaskWidth(0.3F);
-        defaultCenterColorWidth.startShimmerAnimation();
     }
 }
